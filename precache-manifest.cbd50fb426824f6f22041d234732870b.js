760c015588f4576377b1b1a0f7603be5fe1d80f1
Anargu <anthony.arostegui@gmail.com>
2020-06-14 13:59:22 -0500

    dist updated

---
+++ precache-manifest.cbd50fb426824f6f22041d234732870b.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1e9664eb0f6571061bb",
+    "url": "/regosh-landing/main.bundle.3d82ae46fa6fdcf20e11.js"
+  },
+  {
+    "revision": "90c2e7cb37d4aa5ffd76",
+    "url": "/regosh-landing/vendors~main.bundle.ca34426346fc18fc21f8.js"
+  },
+  {
+    "revision": "09f0201e92b42591f3c447f7cd8704aa",
+    "url": "/regosh-landing/assets/file-text.svg"
+  },
+  {
+    "revision": "24a5a3955ab0c15f5d6cd9bffadaf8b5",
+    "url": "/regosh-landing/assets/folder.svg"
+  },
+  {
+    "revision": "5cff78e44ca42e5026f832f7b41fb018",
+    "url": "/regosh-landing/assets/github.svg"
+  },
+  {
+    "revision": "8391957aaff84ed2243736878b5b1108",
+    "url": "/regosh-landing/vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "0f9a5cf89f1d4757a6a4945a329e1fc0",
+    "url": "/regosh-landing/vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "449e154f5a93678178caf6d1d5857d65",
+    "url": "/regosh-landing/vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "abb358dc9afad389c31bee5903095e06",
+    "url": "/regosh-landing/vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "da48dd1cd76dee364cc403d607193f16",
+    "url": "/regosh-landing/vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "893102e1869adc5bce0a94feb6f74249",
+    "url": "/regosh-landing/vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "d42dd0732b85d5efa9e1660a1747b05c",
+    "url": "/regosh-landing/vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "62517b9ba0aa6eb7d9a48cd982b9fa77",
+    "url": "/regosh-landing/assets/eye.svg"
+  },
+  {
+    "revision": "502189aef3b96acbcddedbb54a043638",
+    "url": "/regosh-landing/assets/heart.svg"
+  },
+  {
+    "revision": "195d1b3c4a19ddbfe82d4076e3480d2d",
+    "url": "/regosh-landing/assets/manifest/android-icon-144x144.png"
+  },
+  {
+    "revision": "a4ddfb1091333098737aebbb78cf6e9c",
+    "url": "/regosh-landing/assets/manifest/android-icon-192x192.png"
+  },
+  {
+    "revision": "db891223b9fc9773886d0d2b9e30b103",
+    "url": "/regosh-landing/assets/manifest/android-icon-36x36.png"
+  },
+  {
+    "revision": "07ed692eb45b02ffec7e956044c9b152",
+    "url": "/regosh-landing/assets/manifest/android-icon-72x72.png"
+  },
+  {
+    "revision": "809365f9cdb00278225df88697512ac2",
+    "url": "/regosh-landing/assets/manifest/android-icon-48x48.png"
+  },
+  {
+    "revision": "ba15cd27984421a8f5408f4a24eaec56",
+    "url": "/regosh-landing/assets/manifest/android-icon-96x96.png"
+  },
+  {
+    "revision": "7d841725b87299c0a7469200e971390a",
+    "url": "/regosh-landing/assets/manifest/apple-icon-114x114.png"
+  },
+  {
+    "revision": "d9aa581165c22f18ce2813d591fda5a9",
+    "url": "/regosh-landing/assets/manifest/apple-icon-120x120.png"
+  },
+  {
+    "revision": "195d1b3c4a19ddbfe82d4076e3480d2d",
+    "url": "/regosh-landing/assets/manifest/apple-icon-144x144.png"
+  },
+  {
+    "revision": "d8d08f6a215b0647e74587017c6fc23d",
+    "url": "/regosh-landing/assets/manifest/apple-icon-152x152.png"
+  },
+  {
+    "revision": "03f291ec2d69097dffb125482e34b087",
+    "url": "/regosh-landing/assets/manifest/apple-icon-180x180.png"
+  },
+  {
+    "revision": "cd48f42e14b92ab0fdb3eea7ba80f024",
+    "url": "/regosh-landing/assets/manifest/apple-icon-57x57.png"
+  },
+  {
+    "revision": "bd91af0c7048a76e529f10cbf2d4460a",
+    "url": "/regosh-landing/assets/manifest/apple-icon-60x60.png"
+  },
+  {
+    "revision": "07ed692eb45b02ffec7e956044c9b152",
+    "url": "/regosh-landing/assets/manifest/apple-icon-72x72.png"
+  },
+  {
+    "revision": "c0564a2dcab98e772131d93c0106a12d",
+    "url": "/regosh-landing/assets/manifest/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "dbec4bd7ff4d97aceb91da86e7cd2af6",
+    "url": "/regosh-landing/assets/manifest/apple-icon-76x76.png"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/regosh-landing/assets/manifest/browserconfig.xml"
+  },
+  {
+    "revision": "c0564a2dcab98e772131d93c0106a12d",
+    "url": "/regosh-landing/assets/manifest/apple-icon.png"
+  },
+  {
+    "revision": "33f481e5872cc0d98110c165f3ffb1c1",
+    "url": "/regosh-landing/assets/manifest/favicon-16x16.png"
+  },
+  {
+    "revision": "ba15cd27984421a8f5408f4a24eaec56",
+    "url": "/regosh-landing/assets/manifest/favicon-96x96.png"
+  },
+  {
+    "revision": "cc1165fff3578066663280b819764ceb",
+    "url": "/regosh-landing/assets/manifest/favicon-32x32.png"
+  },
+  {
+    "revision": "477cfaca9467544d83c4e32857bf014f",
+    "url": "/regosh-landing/assets/manifest/favicon.ico"
+  },
+  {
+    "revision": "b58fcfa7628c9205cb11a1b2c3e8f99a",
+    "url": "/regosh-landing/assets/manifest/manifest.json"
+  },
+  {
+    "revision": "195d1b3c4a19ddbfe82d4076e3480d2d",
+    "url": "/regosh-landing/assets/manifest/ms-icon-144x144.png"
+  },
+  {
+    "revision": "29ca796246c469bd391935ef0c695efe",
+    "url": "/regosh-landing/assets/manifest/ms-icon-310x310.png"
+  },
+  {
+    "revision": "23556e675f034e0ed06c96df6e23c33f",
+    "url": "/regosh-landing/assets/manifest/ms-icon-150x150.png"
+  },
+  {
+    "revision": "704b1bfbb64ace2fcd5a5243b6ee729c",
+    "url": "/regosh-landing/assets/manifest/ms-icon-70x70.png"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "/regosh-landing/assets/roboto-mono-v5-latin-regular.woff2"
+  },
+  {
+    "revision": "28b589acbb045a3b59fdef73b2445975",
+    "url": "/regosh-landing/index.html"
+  }
+];
